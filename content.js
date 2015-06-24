79c7c76b560958c0a32ba356e19feb4c70237279
Jeremiah Lee <jeremiah.lee@gmail.com>
2015-06-23 20:08:13 -0700

    small fix.

--- content.js
+++ content.js
@@ -2,6 +2,6 @@ $('.js-issue-title').html($('.js-issue-title').html().replace(/\b([A-Z]+-\d+)/g,
                                                               '<a href="https://hearsaysocial.atlassian.net/browse/$1">$1</a>'));
 
 $('.js-comment-body').each(function() {
-  $(this).html($(this).html().replace(/([^\/"])\b([A-Z]+-\d+)/g,
+  $(this).html($(this).html().replace(/([^\/"\[])\b([A-Z]+-\d+)/g,
                                       '$1<a href="https://hearsaysocial.atlassian.net/browse/$2">$2</a>'));
 });
