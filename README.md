ecf428352d6d061a4f64224ae94bbe1bfa6605c8
Jeremiah Lee <jelee@nerdwallet.com>
2016-08-22 16:52:48 -0700

    Update for my new place of employment.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # jira-link-extension
+
+This is just a little extension for Chrome that will link JIRA ticket
+names in PR bodies and titles to the appropriate JIRA ticket.
+
+Clone this repo, and then install as a Developer-mode Chrome extension.
+Maybe one day I'll get around to actually making it configurable and
+submitting it, but I haven't done so yet.
--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 $('.js-issue-title').html($('.js-issue-title').html().replace(/\b([A-Z]+-\d+)/g,
-                                                              '<a href="https://hearsaysocial.atlassian.net/browse/$1">$1</a>'));
+                                                              '<a href="https://nerdwallet.atlassian.net/browse/$1">$1</a>'));
 
 $('.js-comment-body').each(function() {
   $(this).html($(this).html().replace(/([^\/"\[])\b([A-Z]+-\d+)/g,
-                                      '$1<a href="https://hearsaysocial.atlassian.net/browse/$2">$2</a>'));
+                                      '$1<a href="https://nerdwallet.atlassian.net/browse/$2">$2</a>'));
 });
